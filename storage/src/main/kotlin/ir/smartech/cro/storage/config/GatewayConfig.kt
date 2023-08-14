cd8113e0f726a92586d2f14669cabd8c75cdb40d
kamand <fateme.t@smartech.ir>
2023-08-14 14:31:48 +0330

    + add objectMapper + improve naming + add some kafka listener config + add DataMapper (its empty for now) + add GatewayListener and use @KafkaListener   - it ready for using mapper and saving in clickhouse   - set kafka auto commit on manual mode

--- storage/src/main/kotlin/ir/smartech/cro/storage/config/GatewayConfig.kt
+++ storage/src/main/kotlin/ir/smartech/cro/storage/config/GatewayConfig.kt
@@ -1,8 +1,13 @@
 package ir.smartech.cro.storage.config
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.annotation.EnableKafka
 
 @Configuration
 @EnableKafka
-class GatewayConfig
+class GatewayConfig {
+    @Bean
+    fun objectMapper(): ObjectMapper = ObjectMapper()
+}
