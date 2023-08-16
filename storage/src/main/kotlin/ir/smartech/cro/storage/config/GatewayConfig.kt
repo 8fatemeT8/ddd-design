e4ca59737bed4df88228c6d1305789084a0293b4
kamand <fateme.t@smartech.ir>
2023-08-16 13:39:51 +0330

    + add clickhouseDatasource bean + add ClickhouseMessage and ClickhouseService + add KafkaMessage and change GatewayListener

--- storage/src/main/kotlin/ir/smartech/cro/storage/config/GatewayConfig.kt
+++ storage/src/main/kotlin/ir/smartech/cro/storage/config/GatewayConfig.kt
@@ -1,6 +1,8 @@
 package ir.smartech.cro.storage.config
 
+import cc.blynk.clickhouse.ClickHouseDataSource
 import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.annotation.EnableKafka
@@ -8,6 +10,17 @@ import org.springframework.kafka.annotation.EnableKafka
 @Configuration
 @EnableKafka
 class GatewayConfig {
+
+    @Value("\${spring.datasource.url}")
+    val dataSourceUrl: String? = null
+
     @Bean
     fun objectMapper(): ObjectMapper = ObjectMapper()
+
+    @Bean
+    fun datasource(): ClickHouseDataSource {
+        val datasource= ClickHouseDataSource(dataSourceUrl)
+        datasource.connection.createStatement().execute("CREATE DATABASE IF NOT EXISTS CRO")
+        return datasource
+    }
 }
---
+++ storage/src/main/kotlin/ir/smartech/cro/storage/data/clickhouse/dto/ClickhouseMessage.kt
@@ -0,0 +1,4 @@
+package ir.smartech.cro.storage.data.clickhouse.dto
+
+// TODO add correct fields and structure
+data class ClickhouseMessage(var message: String? = null, var name: String? = null, var age: Int? = null)
