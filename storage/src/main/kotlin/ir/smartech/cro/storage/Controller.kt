154083adfef433e6410e31cf29afdd0f4e408480
kamand <fateme.t@smartech.ir>
2023-08-19 16:55:45 +0330

    analytics:   - exclude snakeyaml from jpa data storage:   - add some kafkaConsumer configs   - add ParquetService and parquet config files   - add schedule method for read and save data from kafka (daily)   - add parquet file schema

--- storage/src/main/kotlin/ir/smartech/cro/storage/Controller.kt
+++ storage/src/main/kotlin/ir/smartech/cro/storage/Controller.kt
@@ -2,6 +2,7 @@ package ir.smartech.cro.storage
 
 import ir.smartech.cro.storage.data.clickhouse.ClickhouseService
 import ir.smartech.cro.storage.data.kafka.dto.KafkaMessage
+import ir.smartech.cro.storage.data.parquet.ParquetService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
@@ -10,10 +11,14 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/web/cro/gateway")
-class Controller(private val service: Service, private val clickhouseService: ClickhouseService) {
+@RequestMapping("/api/web/cro/collector")
+class Controller(
+    private val service: Service,
+    private val clickhouseService: ClickhouseService,
+    private val parquetService: ParquetService
+) {
 
-    @PostMapping("/write")
+    @PostMapping("/receive")
     fun receiveData(@RequestBody message: KafkaMessage) {
         // TODO : add validation, it must save valid data to kafka
         service.writeToKafka(message)
